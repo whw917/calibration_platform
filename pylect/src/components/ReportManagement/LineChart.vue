7732029519bd3ddf08c329dfc10f31e4d8bd64f7
whw917 <63941203+whw917@users.noreply.github.com>
2023-07-10 16:36:59 +0800

    Update LineChart.vue
    -- 基础function完成
    [ ] 最底下的table的问题是不确定平均值与采集值对表的影响

--- pylect/src/components/ReportManagement/LineChart.vue
+++ pylect/src/components/ReportManagement/LineChart.vue
@@ -5,7 +5,6 @@
     <Checkbox v-model="show1">showA</Checkbox>
     <Checkbox v-model="show2">showB</Checkbox>
   </div>
-  
 </template>
 
 <script>
@@ -19,86 +18,83 @@ export default {
       chart: null,
     }
   },
+  watch: {
+    show1() {
+      this.updateChart()
+    },
+    show2() {
+      this.updateChart()
+    },
+  },
   mounted() {
     this.chart = echarts.init(this.$refs.chart)
-    this.chart.setOption({
-      tooltip: {
-        trigger: 'axis'
-      },
-      legend: {
-        data: [
-          { name: '测量值', icon: 'circle' },
-          { name: '参考值', icon: 'circle' },
-          { name: '差距', icon: 'circle' }
-
-        ],
-        itemWidth: 6,
-        itemHeight: 6,
-        itemGap: 30,
-        textStyle: {
-          color: '#333',
-          fontSize: 12,
-          lineHight: 20
-        }
-      },
-      xAxis: [
-        {
-          type: 'category',
-          axisTick: {
-            alignWithLabel: true
-          },
-          axisLine: {
-            onZero: false,
-            lineStyle: {
-              color: '#333'
-            }
-          },
-          //data: [-90, -60,-30,0,30,60,90]
-        }
-      ],
-      yAxis: [
-        {
-          type: 'value',
-          axisLabel: {
-            textStyle: {
-              color: '#333',
-              fontSize:8
-            }
-          }
-        }
-      ],
-      series: [
+    this.updateChart()
+  },
+  methods: {
+    updateChart() {
+      let series = [
         {
           name: '测量值',
           type: 'line',
           smooth: true,
-          data: [[-90,2],[-70,2],[-60,3],[-40,5],[-30,2],[-10,1],[0,2],[10,2],[30,2],[60,5],[70,4],[90,2],],
+          data: [[-90,2],[-70,2],[-60,3],[-40,5],[-30,2],[-10,1],[0,2],[10,2],[30,2],[60,5],[70,4],[90,2]]
+        }
+      ]
 
-          markLine: {
-            lineStyle: {
-              type: 'dashed'
-            },
-            data: [
-              [{ type: 'min' }, { type: 'max' }]
-            ]
-          }
-        },
-        {
+      if (this.show1) {
+        series.push({
           name: '参考值',
           type: 'line',
           smooth: true,
-          data: [[-90,1],[-60,2],[-30,5],[0,3],[30,1],[60,2],[90,3],],
-        },
-        {
+          data: [[-90,1],[-60,2],[-30,5],[0,3],[30,1],[60,2],[90,3]]
+        })
+      }
+
+      if (this.show2) {
+        series.push({
           name: '差距',
           type: 'line',
           smooth: true,
-          data: [[-90,2],[-60,2],[-30,3],[0,4],[30,1],[60,2],[90,2],],
-          
+          data: [[-90,2],[-60,2],[-30,3],[0,4],[30,1],[60,2],[90,2]]
+        })
+      }
+
+      this.chart.setOption({
+        tooltip: {
+          trigger: 'axis'
         },
-        
-      ]
-    })
+        legend: {
+          data: [
+            { name: '测量值', icon: 'circle' },
+            { name: '参考值', icon: 'circle' },
+            { name: '差距', icon: 'circle' }
+          ],
+          itemWidth: 6,
+          itemHeight: 6,
+          itemGap: 30,
+          textStyle: {
+            color: '#333',
+            fontSize: 12,
+            lineHight: 20
+          }
+        },
+        xAxis: [
+          {
+            type: 'value',
+            min: -90,
+            max: 90
+          }
+        ],
+        yAxis: [
+          {
+            type: 'value',
+            min: 0,
+            max: 5
+          }
+        ],
+        series
+      }, true)  // add true here to replace the old option entirely
+    }
   }
 }
 </script>
